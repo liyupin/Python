fd169d039328f84ce6dc68c3351e86150585d244
Administrator <admin@example.com>
2022-03-24 18:03:16 +0800

    Changes

---
+++ 从零学Python-掘金活动/day02/ide-1.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author:liyupi
+@file: ide-1.py
+@time: 2022/03/24
+"""
+#程序主入口
+if __name__ == '__main__':
+    company, date, *others = ['jinlinyun', '20220324', 'night', 'Tues']
+    print(company, date, others)
